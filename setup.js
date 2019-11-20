17531b3e49296bce0562616c9f26a88f70403eec
Jordan Duncan <jduncan@vidatec.com>
2019-11-20 11:24:51 +0000

    chore(detox): remove detox

--- setup.js
+++ setup.js
@@ -16,8 +16,6 @@ scripts.test = 'jest --verbose';
 scripts.test_update = 'jest -u';
 scripts.test_clear = 'jest --clearCache';
 scripts.lint = 'eslint *.js ./ --fix';
-scripts.detox_ios = 'detox build -c ios.sim.debug && detox test -c ios.sim.debug';
-scripts.detox_android = 'detox build -c android.emu.debug && detox test -c android.emu.debug';
 
 json.scripts = scripts;
 
@@ -39,25 +37,6 @@ json.jest = {
     '<rootDir>/__tests__/**/*.test.js?(x)',
     '<rootDir>/app/**/*.test.js'
   ]
-};
-
-console.log('Setting detox...');
-json.detox = {
-  'configurations': {
-    'ios.sim.debug': {
-      'binaryPath': `ios/build/Build/Products/Debug-iphonesimulator/${name}.app`,
-      'build': `xcodebuild -project ios/${name}.xcodeproj -scheme ${name} -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build -UseModernBuildSystem=NO`,
-      'type': 'ios.simulator',
-      'name': 'iPhone 7'
-    },
-    'android.emu.debug': {
-      'binaryPath': 'android/app/build/outputs/apk/debug/app-debug.apk',
-      'build': 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug && cd ..',
-      'type': 'android.emulator',
-      'name': 'Nexus_5X_API_27_x86'
-    }
-  },
-  'test-runner': 'jest'
 };
 
 console.log('Setting rnpm...');
